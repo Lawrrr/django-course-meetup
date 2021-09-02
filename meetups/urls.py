5aa7ed7cdd4f645d259922173e5deaeeee852272
Lawr <lawrboni@gmail.com>
2021-09-03 02:07:31 +0800

    Form Registration

--- meetups/urls.py
+++ meetups/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('meetups/', views.index, name='all-meetups'), # our-domain.com/meetups
-    path('meetups/<slug:meetup_slug>', views.meetup_details, name="meetup-detail") # our-domain.com/meetups/<dynamic-path-segment>
+    path('meetups/', views.index, name='all-meetups'),  # our-domain.com/meetups
+    path('meetups/success', views.confirm_registration,
+         name='confirm-registration'),  # our-domain.com/meetups
+    # our-domain.com/meetups/<dynamic-path-segment>
+    path('meetups/<slug:meetup_slug>',
+         views.meetup_details, name="meetup-detail"),
 ]
--- meetups/views.py
+++ meetups/views.py
@@ -1,9 +1,11 @@
 from django.http.response import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from .forms import RegistrationForm
 from .models import Meetup
 
 # Create your views here.
+
 
 def index(request):
     meetups = Meetup.objects.all()
@@ -12,15 +14,32 @@ def index(request):
         'meetups': meetups
     })
 
+
 def meetup_details(request, meetup_slug):
     try:
         selected_meetup = Meetup.objects.get(slug=meetup_slug)
+        if request.method == 'GET':
+            registration_form = RegistrationForm()
+        else:
+            registration_form = RegistrationForm(request.POST)
+            if registration_form.is_valid():
+                participant = registration_form.save()
+                selected_meetup.participants.add(participant)
+
+                return redirect('confirm-registration')
 
         return render(request, 'meetups/meetup-details.html', {
             'meetup_found': True,
-            'meetup': selected_meetup
+            'meetup': selected_meetup,
+            'form': registration_form
         })
-    except:
+
+    except Exception as exc:
+        print(exc)
         return render(request, 'meetups/meetup-details.html', {
             'meetup_found': False
         })
+
+
+def confirm_registration(request):
+    return render(request, 'meetups/registration-success.html')
